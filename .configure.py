4c0ed7c9ae83b5fb52f91ceb3bd3e9276d87e201
simuns <simunhojgaard@gmail.com>
2022-08-13 17:45:27 +0200

    Added service creation functions

--- .configure.py
+++ .configure.py
@@ -9,6 +9,10 @@ import os
 from os import path # Check if file exsists
 import shutil # Copy files
 
+import getpass # Used to retrieve username
+import pwd # Used to retrieve username
+
+
 def execute(cmd):
     exec = subprocess.Popen(cmd, stdout=PIPE, stderr=STDOUT, shell=True)
     output = exec.communicate()[0]
@@ -70,6 +74,52 @@ def install_dependencies(distro, missing_Dependencies):
             sys.exit()
 
 
+def get_user():
+    """Try to find the user who called sudo/pkexec."""
+    try:
+        return os.getlogin()
+    except OSError:
+        # failed in some ubuntu installations and in systemd services
+        pass
+
+    try:
+        user = os.environ['USER']
+    except KeyError:
+        # possibly a systemd service. no sudo was used
+        return getpass.getuser()
+
+    if user == 'root':
+        try:
+            return os.environ['SUDO_USER']
+        except KeyError:
+            # no sudo was used
+            pass
+
+        try:
+            pkexec_uid = int(os.environ['PKEXEC_UID'])
+            return pwd.getpwuid(pkexec_uid).pw_name
+        except KeyError:
+            # no pkexec was used
+            pass
+
+    return user
+
+def prepare_service():
+    print("Preparing service")
+    user = get_user()
+
+    fin = open("screenSelector.service.template", "rt")
+    #output file to write the result to
+    fout = open("screenSelector.service", "wt")
+    #for each line in the input file
+    for line in fin:
+    	#read replace the string and write to output file
+    	fout.write(line.replace('USER', user))
+    #close input and output files
+    fin.close()
+    fout.close()
+    return
+
 def install_bin(install_dest, exe_path):
     print("Creating installation directory...")
     execute(f"mkdir -p {install_dest}/screenSelector-x11")
@@ -81,13 +131,14 @@ def install_bin(install_dest, exe_path):
         os.symlink(f"{install_dest}/screenSelector-x11/screenSelector.py", f"{exe_path}/screenSelector")
     except:
         pass
+    execute("systemctl daemon-reload")
 
 def main():
     get_privelege()
     missing_Dependencies = list_missingDependencies()
     distro = check_linuxFlavor()
     install_dependencies(distro, missing_Dependencies)
-
+    prepare_service()
     install_dest = ("/usr/share/pyshared")
     exe_path = ("/usr/local/bin")
     install_bin(install_dest, exe_path)
